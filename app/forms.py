c3b5235b8d7c5aa81f11a22f7289bba4dc872c5f
princeloki <teriqstrachan@yahoo.com>
2023-02-25 17:02:39 -0500

    Login and upload feature works

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
+
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from flask_wtf.file import FileAllowed 
+from wtforms import StringField, PasswordField, FileField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    
+class UploadForm(FlaskForm):
+    file = FileField('image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only!'),
+        InputRequired()
+    ])
